d212afdcaa72260e70f7f223f42cad10ff7c2331
Gautham Krishna J L <gauthamkrishnalj@gmail.com>
2025-11-26 23:50:50 +0530

    feat: add auto detect device type
    Co-authored-by: itsAnanth <ananthsankar2003@gmail.com>

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,6 +6,23 @@ import os
 import torch
 import pickle as pkl
 
+import torch
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+def autodetect_device():
+    if torch.cuda.is_available():
+        device_type = 'cuda'
+    elif torch.backends.mps.is_available():
+        device_type = 'mps'
+    else:
+        device_type = 'cpu'
+        
+    logger.info(f"Autodetected device type as {device_type}")
+    return device_type
+
 def set_seed(seed): 
     random.seed(seed)
     np.random.seed(seed)
